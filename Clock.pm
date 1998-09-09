443af5ea1ba35c332fc59b4c0ad1c6124108ebc0
H.Merijn Brand <h.m.brand@xs4all.nl>
1998-09-09 14:45:06 +0000

    Import of HMBRAND/Tk-Clock-0.03 from CPAN.
    gitpan-cpan-distribution: Tk-Clock
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         HMBRAND/Tk-Clock-0.03.tgz
    gitpan-cpan-author:       HMBRAND
    gitpan-cpan-maturity:     released

--- Clock.pm
+++ Clock.pm
@@ -10,6 +10,8 @@ use Tk::Clock;
 $clock = $parent->Clock (?-option => <value> ...?);
 
 $clock->config (	# These reflect the defaults
+    useAnalog	=> 1,
+    useDigital	=> 1,
     handColor	=> "Green4",
     secsColor	=> "Green2",
     tickColor	=> "Yellow4",
@@ -22,7 +24,8 @@ $clock->config (	# These reflect the defaults
 
 =head1 DESCRIPTION
 
-Create a clock canvas with both an analog- and a digital display
+Create a clock canvas with both an analog- and a digital display. Either
+can be disabled by setting useAnalog or useDigital to 0 resp.
 
 Legal dateFormat characters are d and dd for date, m and mm for month,
 y and yy for year and any separators :, -, / or space.
@@ -46,7 +49,7 @@ There's no check if either format will fit in the given space.
 =head1 TODO
 
 * Scalability would be a nice feature.
-* Using POSIX' strftima () for dateFormat. Current implementation
+* Using POSIX' strftime () for dateFormat. Current implementation
   would probably make this very slow.
 
 =head1 AUTHOR
@@ -71,7 +74,7 @@ use Tk::Derived;
 use Tk::Canvas;
 
 @ISA = qw/Tk::Derived Tk::Canvas/;
-$VERSION = "0.02";
+$VERSION = "0.03";
 
 Construct Tk::Widget "Clock";
 
@@ -88,6 +91,9 @@ my %def_config = (
     dateColor	=> "Blue4",
     dateFormat	=> "dd-mm-yy",
 
+    useDigital	=> 1,
+    useAnalog	=> 1,
+
     fmtd	=> sub {
 	my ($d, $m, $y) = @_;
 	sprintf "%02d-%02d-%02d", $d, $m + 1, $y;
@@ -97,6 +103,86 @@ my %def_config = (
 	sprintf "%02d.%02d:%02d", $h, $m, $s;
 	},
     );
+
+sub resize ($)
+{
+    my $clock = shift;
+
+    my $data = $clock->privateData;
+    my $hght = $data->{useAnalog}  * 73 + $data->{useDigital} * 26 + 1;
+    $clock->cget (-height) == $hght && return;
+    $clock->configure (-height => $hght);
+    } # resize
+
+sub createDigital ($)
+{
+    my $clock = shift;
+
+    my $data = $clock->privateData;
+    my $off = $data->{useAnalog} * 73;
+    $clock->createText (37, $off + 26,
+	-width  => 65,
+	-font   => $data->{dateFont},
+	-fill   => $data->{dateColor},
+	-text   => $data->{dateFormat},
+	-tags   => "date",
+	-anchor => "s");
+    $clock->createText (37, $off + 13,
+	-width  => 65,
+	-font   => $data->{timeFont},
+	-fill   => $data->{timeColor},
+	-text   => $data->{timeFormat},
+	-tags   => "time",
+	-anchor => "s");
+    $data->{Clock_h} = -1;
+#   $data->{Clock_m} = -1;
+    $data->{Clock_s} = -1;
+    $clock->resize ($data);
+    } # createDigital
+
+sub destroyDigital ($)
+{
+    my $clock = shift;
+
+    $clock->delete ("date");
+    $clock->delete ("time");
+    } # destroyDigital
+
+sub createAnalog ($)
+{
+    my $clock = shift;
+
+    my $data = $clock->privateData;
+
+    foreach my $tick (0 .. 59) {
+	my $l = $tick % 15 == 0 ? 28 :
+		$tick %  5 == 0 ? 30 : 33;
+	my $a = $tick * .104720;
+	my $x = sin ($a);
+	my $y = cos ($a);
+	$clock->createLine (
+	    36 + $x * $l, 36 - $y * $l,
+	    36 + $x * 35, 36 - $y * 35,
+	    -tags  => "tick",
+	    -arrow => "none",
+	    -fill  => $data->{tickColor},
+	    -width => 1.0);
+	}
+    $data->{Clock_h} = -1;
+    $data->{Clock_m} = -1;
+    $data->{Clock_s} = -1;
+    $clock->resize ($data);
+    } # createAnalog
+
+sub destroyAnalog ($)
+{
+    my $clock = shift;
+
+    $clock->delete ("tick");
+    $clock->delete ("hour");
+    $clock->delete ("min");
+    $clock->delete ("sec");
+    } # destroyAnalog
 
 sub Populate
 {
@@ -119,36 +205,9 @@ sub Populate
         -takefocus          => [ qw(SELF takefocus          Takefocus          0     ) ],
         );
 
-    $clock->createText (37, 99,
-        -width  => 65,
-        -font   => $data->{dateFont},
-        -fill   => $data->{dateColor},
-        -text   => $data->{dateFormat},
-        -tags   => "date",
-        -anchor => "s");
-    $clock->createText (37, 86,
-        -width  => 65,
-        -font   => $data->{timeFont},
-        -fill   => $data->{timeColor},
-        -text   => $data->{timeFormat},
-        -tags   => "time",
-        -anchor => "s");
-
-    foreach my $tick (0 .. 59) {
-        my $l = $tick % 15 == 0 ? 28 :
-		$tick %  5 == 0 ? 30 : 33;
-        my $a = $tick * .104720;
-        my $x = sin ($a);
-        my $y = cos ($a);
-        $clock->createLine (
-            36 + $x * $l, 36 - $y * $l,
-            36 + $x * 35, 36 - $y * 35,
-            -tags  => "tick",
-            -arrow => "none",
-            -fill  => $data->{tickColor},
-            -width => 1.0);
-	}
-
+    $data->{useDigital} && $clock->createDigital;
+    $data->{useAnalog}  && $clock->createAnalog;
+	
     $clock->repeat (995, ["run" => $clock]);
     } # Populate
 
@@ -174,6 +233,8 @@ sub config ($@)
     my $data = $clock->privateData;
     foreach my $conf_spec (keys %def_config) {
 	defined $conf->{$conf_spec} || next;
+	defined $data->{$conf_spec} || next;
+	my $old = $data->{$conf_spec};
 	$data->{$conf_spec} = $conf->{$conf_spec};
 	if    ($conf_spec eq "tickColor") {
 	    $clock->itemconfigure ("tick", -fill => $data->{tickColor});
@@ -251,10 +312,34 @@ sub config ($@)
 		}
 	    $data->{fmtt}=eval"sub{my(\$H,\$M,\$S)=\@_;my\$h=\$H%12;my\$A=\$H>11?'PM':'AM';sprintf \"$fmt\"$arg;}";
 	    }
+	elsif ($conf_spec eq "useAnalog") {
+	    if    ($old == 1 && $data->{useAnalog} == 0) {
+		$clock->destroyAnalog;
+		$clock->destroyDigital;
+		$data->{useDigital} && $clock->createDigital;
+		}
+	    elsif ($old == 0 && $data->{useAnalog} == 1) {
+		$clock->destroyDigital;
+		$clock->createAnalog;
+		$data->{useDigital} && $clock->createDigital;
+		}
+	    $clock->resize;
+	    $clock->after (5, ["run" => $clock]);
+	    }
+	elsif ($conf_spec eq "useDigital") {
+	    if    ($old == 1 && $data->{useDigital} == 0) {
+		$clock->destroyDigital;
+		}
+	    elsif ($old == 0 && $data->{useDigital} == 1) {
+		$clock->createDigital;
+		}
+	    $clock->resize;
+	    $clock->after (5, ["run" => $clock]);
+	    }
 	}
     } # config
 
-sub where
+sub where ($$$)
 {
     my ($clock, $tick, $len) = @_;      # ticks 0 .. 59
     my ($x, $y, $a);
@@ -265,7 +350,7 @@ sub where
     (36 - $x / 4, 36 + $y / 4, 36 + $x, 36 - $y);
     } # where
 
-sub run
+sub run ($)
 {
     my $clock = shift;
 
@@ -275,39 +360,46 @@ sub run
 
     unless ($t[2] == $data->{Clock_h}) {
 	$data->{Clock_h} = $t[2];
-	$clock->itemconfigure ("date", -text => &{$data->{fmtd}} (@t[3,4,5]));
+	$data->{useDigital} &&
+	    $clock->itemconfigure ("date",
+		-text => &{$data->{fmtd}} (@t[3,4,5]));
 	}
 
     unless ($t[1] == $data->{Clock_m}) {
         $data->{Clock_m} = $t[1];
-        $clock->delete ("hour");
-        $clock->createLine (
-	    $clock->where (($data->{Clock_h} % 12) * 5 + $t[1] / 12, 22),
-		-tags  => "hour",
-		-arrow => "none",
-		-fill  => $data->{handColor},
-		-width => 2.6);
+	if ($data->{useAnalog}) {
+	    $clock->delete ("hour");
+	    $clock->createLine (
+		$clock->where (($data->{Clock_h} % 12) * 5 + $t[1] / 12, 22),
+		    -tags  => "hour",
+		    -arrow => "none",
+		    -fill  => $data->{handColor},
+		    -width => 2.6);
 
-        $clock->delete ("min");
-        $clock->createLine (
-	    $clock->where ($data->{Clock_m}, 30),
-		-tags  => "min",
-		-arrow => "none",
-		-fill  => $data->{handColor},
-		-width => 1.5);
-        }
+	    $clock->delete ("min");
+	    $clock->createLine (
+		$clock->where ($data->{Clock_m}, 30),
+		    -tags  => "min",
+		    -arrow => "none",
+		    -fill  => $data->{handColor},
+		    -width => 1.5);
+	    }
+	}
 
     unless ($t[0] == $data->{Clock_s}) {
         $data->{Clock_s} = $t[0];
-        $clock->delete ("sec");
-        $clock->createLine (
-	    $clock->where ($data->{Clock_s}, 34),
-		-tags  => "sec",
-		-arrow => "none",
-		-fill  => $data->{secsColor},
-		-width => 0.8);
-
-	$clock->itemconfigure ("time", -text => &{$data->{fmtt}} (@t[2,1,0]));
+        if ($data->{useAnalog}) {
+	    $clock->delete ("sec");
+	    $clock->createLine (
+		$clock->where ($data->{Clock_s}, 34),
+		    -tags  => "sec",
+		    -arrow => "none",
+		    -fill  => $data->{secsColor},
+		    -width => 0.8);
+	    }
+	$data->{useDigital} &&
+	    $clock->itemconfigure ("time",
+		-text => &{$data->{fmtt}} (@t[2,1,0]));
         } 
     } # run
 
--- t/clock.t
+++ t/clock.t
@@ -6,13 +6,13 @@ END {
     $::loaded || print "not ok 1\n";
     }
 
-print "1..2\n";
+print "1..6\n";
 use Tk;
 use Tk::Clock;
 $::loaded = 1;
 print "ok 1\n";
 
-print "2..2\n";
+print "2..6\n";
 my $m = MainWindow->new (-title => "clock");
 my $c = $m->Clock (-background => "Black");
 $c->config (
@@ -24,22 +24,36 @@ $c->config (
     timeFont  => "-misc-fixed-medium-r-normal--13-*-75-75-c-*-iso8859-1",
     );
 $c->pack;
-sub stop_clock
-{
-    $c->destroy;
-    print "ok 2\n";
-    $m->destroy;
-    } # stop_clock
+print "ok 2\n";
+print "3..6\n";
 
-sub chg_clock
-{
+$c->after ( 5000, sub {
+    $c->config (useAnalog => 1, useDigital => 0);
+    print "ok 3\n";
+    print "4..6\n";
+    }); # no_analog
+
+$c->after (10000, sub {
+    $c->config (useAnalog => 0, useDigital => 1);
+    print "ok 4\n";
+    print "5..6\n";
+    }); # no_analog
+
+$c->after (15000, sub {
     $c->config (
+	useAnalog => 1,
+	useDigital=> 1,
 	dateFormat=> "m/d/y",
 	timeFormat=> "hh:MM A",
 	);
-    }
+    print "ok 5\n";
+    print "6..6\n";
+    }); # clock_us
 
-$c->after ( 5000, \&chg_clock);
-$c->after (10000, \&stop_clock);
+$c->after (20000, sub {
+    $c->destroy;
+    $m->destroy;
+    print "ok 6\n";
+    }); # stop_clock
 
 MainLoop;
