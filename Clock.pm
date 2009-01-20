b0ade8f133b91acfe74b206c5bdf100de1dc3834
H.Merijn Brand <h.m.brand@xs4all.nl>
2009-01-20 17:15:42 +0000

    Import of HMBRAND/Tk-Clock-0.24 from CPAN.
    gitpan-cpan-distribution: Tk-Clock
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         HMBRAND/Tk-Clock-0.24.tgz
    gitpan-cpan-author:       HMBRAND
    gitpan-cpan-maturity:     released

--- Clock.pm
+++ Clock.pm
@@ -5,7 +5,7 @@ package Tk::Clock;
 use strict;
 use warnings;
 
-our $VERSION = "0.23";
+our $VERSION = "0.24";
 
 use Carp;
 
@@ -675,7 +675,7 @@ Thanks to all who have given me feedback.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 1999-2008 H.Merijn Brand
+Copyright (C) 1999-2009 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
